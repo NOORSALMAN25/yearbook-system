0f8c8016314cd6fa7eb0cc3dffafdb5c605743d9
Hussain-Bader <7ussain.3ali@gmail.com>
2025-07-20 01:19:57 +0300

    authCtrl

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  role: { type: String, required: true },
+  role: { type: String, enum:['teacher','student'],required: true },
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
 })
 
 const User = mongoose.model('User', UserSchema)
