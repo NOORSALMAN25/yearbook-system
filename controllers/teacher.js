68fffc5d06d0c044e92433db3b931e3b47aed11d
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-07-20 09:49:10 +0300

    teacher aspect

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -1,2 +1,15 @@
 const Teacher = require('../models/User')
+const Post = require('../models/Post')
 
+exports.teacher_show_get = async (req, res) => {
+  const teacher = await Teacher.findById(req.params.id)
+  const posts = await Post.find({ author: user._id })
+  const data = {
+    role: 'teacher',
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+    
+  }
+  res.render('./users/profile.ejs', { user: data })
+}
--- routes/teacher.js
+++ routes/teacher.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const teacherCtrl = require('../controllers/teacher')
 
-router.get('/')
+router.get('/:id', teacherCtrl.teacher_show_get)
 
 module.exports = router
