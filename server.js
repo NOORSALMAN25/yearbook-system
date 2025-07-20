00ecabd959d5913561b212fd53450abe3c47c0ad
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-07-20 14:30:35 +0300

    just checking

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const PORT = process.env.PORT ? process.env.PORT : 3000
 
 const app = express()
 
-
-const passUserToView = require('./middleware/pass-user-to-view')
-const isSignedIn = require('./middleware/is-signed-in')
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -26,40 +23,6 @@ app.use(
   })
 )
 
-app.use(passUserToView)
-
-// storage
-const Storage = multer.diskStorage({
-  destination: 'uploads',
-  filename: (req, file, cb) => {
-    cb(null, file.originalname)
-  }
-})
-
-const uploads = multer({
-  storage: Storage
-}).single('pfpImage')
-
-app.post('/upload', (req, res) => {
-  uploads(req, res, (err) => {
-    if (err) {
-      console.log
-    } else {
-      const newImage = new User({
-        role: req.body.role,
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        pfp: {
-          data: req.file.filename,
-          contentType: 'image/png'
-        }
-      })
-      newImage.save().then(() => res.send('successfully uploaded'))
-    }
-  })
-})
-
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
@@ -67,10 +30,12 @@ app.get('/', (req, res) => {
 // require routes
 const authRouter = require('./routes/auth.js')
 const userRouter = require('./routes/user.js')
+const isSignedIn = require('./middleware/is-signed-in.js')
 
 // use routes
-app.use('/user', userRouter)
-app.use('/auth', isSignedIn, authRouter)
+app.use('/auth', authRouter)
+app.use('/user', isSignedIn, userRouter)
+
 
 app.listen(PORT, () => {
   console.log(`running sever on port no. ${PORT} . . . `)
