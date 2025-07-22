e566932a1ab40ed2e213d4688fe5f4972564bbd8
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-07-22 20:34:54 +0300

    view all students and delete any feature

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -1,12 +1,19 @@
 const User = require('../models/User')
 const Post = require('../models/Post')
 
-exports.teacher_edit= async(req , res)=>{
-//   const ifTeacher = await User.findById(req.session.user.id)
-// if (ifTeacher.role === 'teacher'){
-// res.render('user/teacherEdit.ejs', { ifTeacher })
-// }
-const remove = await User.find({})
-console.log('remove', remove)
+exports.teacher_index_get = async (req, res) => {
+  const users = await User.find({})
+  const students = []
+  users.forEach((user) => {
+    if (user.role === 'student') {
+      students.push(user)
+    }
+  })
+  res.render('user/teacher.ejs', {students})
+}
 
+
+exports.teacher_delete_delete = async(req, res)=> {
+  const user = await User.findByIdAndDelete(req.params.id)
+  res.redirect('/teacher')
 }
